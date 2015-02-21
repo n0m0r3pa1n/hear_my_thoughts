051507f6676c86c23db87591144f1fe8230cc1c2
Georgi Mirchev <gmirchev90@gmail.com>
2015-02-21 21:30:22 +0200

    Init commit

---
+++ app/src/main/java/com/nmp90/hearmythoughts/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.nmp90.hearmythoughts.constants;
+
+/**
+ * Created by nmp on 2/21/15.
+ */
+public interface Constants {
+    public static final int SPEECH_COMPLETE_SILENCE = 1000;
+    public static final int SPEECH_COMPLETE_POSSIBLY_COMPLETE_SILEMNCE = 1000;
+    public static final int SPEECH_MINIMUM_LENGTH = 2000;
+    public static final int SPEECH_MAX_RESULTS = 5;
+    public static final String SPEECH_DEFAULT_LANGUAGE = "en";
+}
